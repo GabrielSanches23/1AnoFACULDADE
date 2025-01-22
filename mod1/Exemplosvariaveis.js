7442cd212fa6695bf1e8c9bffcb8cac5ba2afaa0
Gabriel Sanches <cidaetutu42@gmail.com>
2025-01-21 21:32:21 -0300

    first commit

---
+++ mod1/Exemplosvariaveis.js
@@ -0,0 +1,9 @@
+const externa ="Ola Mundo , Gabriel entrando no seu "; 
+console.log (externa)
+function exemplovar() {
+    if (true) {
+        var mensagen = "ola faculdade,sou uma var "
+    } 
+    console.log (mensagen)
+}
+exemplovar()
